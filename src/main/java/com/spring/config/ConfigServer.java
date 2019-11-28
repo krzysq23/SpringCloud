595676ed54297939c372353c56cca707c42c2e6e
krzysq23 <krzysztof.skarbowski@gmail.com>
2019-11-28 10:15:04 +0100

    Adding ribbon

--- src/main/java/com/spring/config/ConfigServer.java
+++ src/main/java/com/spring/config/ConfigServer.java
@@ -2,11 +2,13 @@ package com.spring.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 @ComponentScan(basePackages = { "com.spring.*"})
 public class ConfigServer {
 
--- src/main/java/com/spring/controller/ConfigClientController.java
+++ src/main/java/com/spring/controller/ConfigClientController.java
@@ -1,28 +1,32 @@
 package com.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.sql.DataSource;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @RestController
 @RefreshScope
 public class ConfigClientController {
 
-    @Value("${msg:Hello world - Config Server is not working..pelase check}")
-    private String msg;
+    @Autowired
+    Environment environment;
 
-    @GetMapping("/message")
-    String property() {
-        return msg;
+    @GetMapping("/")
+    public String health() {
+        return "I am Ok";
+    }
+
+    @GetMapping("/backend")
+    public String backend() {
+        System.out.println("Inside MyRestController::backend...");
+
+        String serverPort = environment.getProperty("local.server.port");
+
+        System.out.println("Port : " + serverPort);
+
+        return "Hello form Backend!!! " + " Host : localhost " + " :: Port : " + serverPort;
     }
 
 }
